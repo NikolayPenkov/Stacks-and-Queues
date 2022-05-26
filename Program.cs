a0af40aa7894bb7af5e6917dbfca9200c89e30ec
Nikolay Penkov <97218864+NikolayPenkov@users.noreply.github.com>
2022-05-26 07:12:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace e06.songsQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> songArray = Console.ReadLine().Split(", ").ToList();
+            Queue<string> songs = new Queue<string>(songArray);
+            while (songs.Count>0)
+            {
+                string command = Console.ReadLine();
+                if (command == "Play")
+                {
+                    songs.Dequeue();
+                }
+                else if (command.Contains("Add"))
+                {
+                    string song = command.Substring(4);
+                    if (!songs.Contains(song))
+                    {
+                    songs.Enqueue(song);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                    }
+                }
+                else if (command == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", songs));
+                }
+                if (songs.Count < 1)
+                {
+                    Console.WriteLine("No more songs!");
+                    break;
+                }
+            }
+        }
+    }
+}
